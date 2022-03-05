ba1ce7eb1065448e99b9b087ebd6c8f1d52e5680
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2022-03-05 03:24:40 -0600

    Powershell 3

---
+++ tarea3.ps1
@@ -0,0 +1,39 @@
+﻿
+
+$serviceName = 'wuauserv'
+$serviceInfo = Get-Service -Name $serviceName
+
+if($serviceInfo.Status -eq "Running")
+{
+   
+    Write-Host "El servicio " $serviceName " está ejecutándose"
+}
+
+
+if($serviceInfo.Status -eq "Running")
+{
+ 
+    Write-Host "El servicio " $serviceName " está ejecutándose"
+}
+else{
+    Write-Host "El servicio " $serviceName " está detenido"
+}
+
+
+if($serviceInfo.Status -eq "Running")
+{
+    
+    Write-Host "El servicio " $serviceName " está ejecutándose"
+    Stop-Service -Name $serviceName
+}
+elseif($serviceInfo.Status -eq "Stopped"){
+    Write-Host "El servicio " $serviceName " está detenido"
+    Start-Service -Name $serviceName
+}
+else{
+    Write-Host "El servicio " $serviceName " está suspendido"
+}
+
+Write-Host "Fin!"
+Get-Service -Name $serviceName
+Write-Host $serviceInfo.Status
